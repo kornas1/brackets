0ae5c8f991809b73126a754817f23ce42487ea51
kornas1 <79769071+kornas1@users.noreply.github.com>
2021-03-12 00:59:17 +0300

    feat: add file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  let a = [];
+  let step = 0;
+  
+  while(step == 0) {
+      step = 1;
+      
+      for(let i = 0; i < bracketsConfig.length; i++) {
+          a[i] = bracketsConfig[i][0] + bracketsConfig[i][1];
+          arr = str.split(a[i]);
+          console.log(arr)
+          if(arr.length > 1) {
+              step = 0;
+          }
+          str = arr.join('');
+      }
+  }
+  return str === '' ? true : false
+}
 }
